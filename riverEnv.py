fe7c748cd91280f15cb43c3b4adba5492ecb686f
tboulet <timothe.boulet0@gmail.com>
2022-07-15 02:39:18 +0200

    river env clarified

--- riverEnv.py
+++ riverEnv.py
@@ -1,39 +1,42 @@
 from utils import *
 import gym
-from gym import spaces
-import numpy as np
 
 class RiverEnv(gym.Env):
 
     def __init__(self):
-        self.action_space = spaces.Discrete(2)
-        self.observation_space = spaces.Discrete(11)
+        self.action_space = gym.spaces.Discrete(2)
+        self.observation_space = gym.spaces.Discrete(11)
         super().__init__()
 
     def reset(self) -> Observation:
         self.state = 10
-        return 10
+        return self.state
 
     def step(self, action: Action) -> Tuple[Observation, Reward, bool]:
         assert action == 0 or action == 1, "Action must be in {0, 1} for the riverEnv environnement."
         assert 1 <= self.state <= 10, "The agent should be between 1 and 10 meters when step is called."
         
-        if action == 0: action = -1
-        self.state += action
-        self.state = min(self.state, 10)    #The agent can't go farer than 10 meters.
-        reward = -1
-        info  = dict()
-        if self.state == 0:
-            done = True
-        else:
-            done = False
+        # Action has effect on the environment
+        if action == 0:
+            self.state -= 1
+        elif action == 1:
+            self.state += 1
+            if self.state > 10: self.state = 10
         
-        return self.state, reward, done, info
+        # Compute reward
+        reward = -1
+
+        # Check if env is terminated
+        done = self.state <= 0
+
+        return self.state, reward, done, {}
 
     def render(self):
         print(f"L'agent est à {self.state} mètres de la rive.")
 
 
+
+import numpy as np
 transition_probability = np.array([[[0 for _ in range(11)] for _ in range(2)] for _ in range(11)])
 reward_probability = np.array([[0 for _ in range(2)] for _ in range(11)])
 env = RiverEnv()
